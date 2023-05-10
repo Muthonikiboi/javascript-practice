527b0904836424b19f87d9215c3d1524092e983b
Muthonikiboi <kiboijoye254@gmail.com>
2023-05-10 22:12:29 +0300

    second commit

--- arrays/findIndex.js
+++ arrays/findIndex.js
@@ -31,3 +31,17 @@ function isEngineer(worker){
 console.log(myCompany.findIndex(isEngineer));
 
 console.log(myCompany[myCompany.findIndex(isEngineer)]);
+
+//to get the age of the family
+
+const family = [
+  { name: "Lilian", age: 17 },
+  { name: "Elijah", age: 18 },
+  { name: "Joy", age: 21 },
+  { name: "Emmanuel", age: 22 },
+];
+function isAdult(member) {
+  return member.age >= 18;
+}
+console.log(family.findIndex(isAdult));
+console.log(family[family.findIndex(isAdult)]);
