6eb446f0bd5bb9a4c187eb6f2cf0057cb361a0a2
Muthonikiboi <kiboijoye254@gmail.com>
2023-05-20 20:51:15 +0300

    second commit

---
+++ arrays/includes.js
@@ -0,0 +1,6 @@
+//example 1
+let family_siblings=["Faith","Emmanuel","Joy","Elijah","Lilian","David"];
+let check1=family_siblings.includes("Joy");
+console.log(check1);
+let check2=family_siblings.includes("Gift");
+console.log(check2);
