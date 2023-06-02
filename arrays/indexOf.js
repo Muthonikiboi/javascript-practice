69589a2884252693ba1fb7e0dd8d0a5c97666e47
Muthonikiboi <kiboijoye254@gmail.com>
2023-06-02 23:30:02 +0300

    second commit

--- arrays/indexOf.js
+++ arrays/indexOf.js
@@ -8,3 +8,16 @@ console.log(findname);
 //if no existence of an element
 find_name =names.indexOf('Faith');
 console.log(find_name);
+
+//the second example with if ...else
+
+const salaries = [1000000, 200000, 30000, 40000];
+console.log(salaries.indexOf(1000000));
+
+const salary = salaries.indexOf(40000);
+
+if (salary > -1) {
+  console.log("Salary exists");
+} else {
+  console.log("Salary does not exist");
+}
