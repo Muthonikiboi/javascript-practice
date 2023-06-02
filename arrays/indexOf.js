00aeb6f0e6bb9a91e4d53d9a3dbbd71a411fdd3a
Muthonikiboi <kiboijoye254@gmail.com>
2023-06-02 23:21:42 +0300

    second commit

---
+++ arrays/indexOf.js
@@ -0,0 +1,10 @@
+//the array index finds the first occurence of an element searched
+var names=['Joy','Lilian','David']
+
+//if the searched element is in the array we get the index else -1
+findname =names.indexOf('Joy');
+console.log(findname);
+
+//if no existence of an element
+find_name =names.indexOf('Faith');
+console.log(find_name);
