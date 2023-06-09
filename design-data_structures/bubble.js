82068092724f0ce32aba180d55ecfa67cdfa4f5c
Muthonikiboi <kiboijoye254@gmail.com>
2023-06-09 23:13:12 +0300

    second commit

--- design-data_structures/bubble.js
+++ design-data_structures/bubble.js
@@ -6,7 +6,6 @@ function bubble(array){
     for(let i=0; i<array.length; i++){
         for(let j=0; j<array.length; j++){
             if( array[j] > array[j+1]){
-
                 const index = array[j];
                 array[j] = array[j+1];
                 array[j+1] = index
---
+++ leet&.../analysis1.js
@@ -0,0 +1,24 @@
+const arr= [-5,7,-6,-2,1,3,8,4];
+
+
+function sort_square(arr){
+    const len=arr.length;
+
+    for (let i=0; i<len; i++){
+        for (let j=0; j<len; j++){
+            if(arr[j]>arr[j+1]){
+                const temp =arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp
+            }
+        }
+    }
+    return arr;
+}
+
+sort_square(arr);
+
+function squares(el){ 
+    console.log(el*el);
+}
+arr.forEach(squares);
