4c1065ee3d19bff9b5f46db6eb82df12671532fe
Muthonikiboi <kiboijoye254@gmail.com>
2023-06-11 00:00:06 +0300

    second commit

--- math js/square.js
+++ math js/square.js
@@ -1,16 +1,5 @@
 const number = [5,7,9,34,48,32];
 
-for (var i = 0; i < number.length; i++) {
-     console.log= number[i]** 2;
-}
+function number()
 
-console.log(number);
-
-const numbers = [5, 7, 9, 34, 48, 32];
-
-for (var i = 0; i < numbers.length; i++) {
-  console.log(numbers[i] ** 2);
-}
-
-console.log(numbers);
 
