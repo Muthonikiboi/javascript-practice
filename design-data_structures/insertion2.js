2720bdac9cccc18f804540bd667089f6c78fbd16
Muthonikiboi <kiboijoye254@gmail.com>
2023-06-13 22:27:27 +0300

    second commit

--- design-data_structures/insertion2.js
+++ design-data_structures/insertion2.js
@@ -11,4 +11,7 @@ function insertionSort(arr) {
   }
   console.log(insertionSort([2, 1, 3, 7, 5])) // [1, 2, 3, 5, 7]
 
+  const arr=[6,7,0,4,5,1,3,2];
+  console.log(arr);
+  insertionSort(arr);
   console.log(arr);
