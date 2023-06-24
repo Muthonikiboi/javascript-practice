3ce5126dc0094b44745692ca19413a96b0e44b62
Muthonikiboi <kiboijoye254@gmail.com>
2023-06-24 23:38:08 +0300

    array push

---
+++ arrays/push().js
@@ -0,0 +1,19 @@
+//EXAMPLE 1
+let arr =["Faith","Emmanuel","Elijah","Joy"];
+
+arr.push("Lilian","David");
+console.log(arr);
+
+console.log(arr.push());
+
+//EXAMPLE 2
+function func(){
+
+let arr =["Faith","Emmanuel","Elijah","Joy"];
+
+arr.push("Lilian","David");
+console.log(arr);
+
+console.log(arr.push());
+}
+func();
