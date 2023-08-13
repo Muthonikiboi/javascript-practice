bb1a7a15a772b11b16dec02f6f954bded63e42ab
Muthonikiboi <kiboijoye254@gmail.com>
2023-08-13 13:47:45 +0300

    jonas challenge 2

---
+++ jonas_complete_js/challenge2.js
@@ -0,0 +1,45 @@
+/*Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, telling the user who has the higher BMI. The message can be either:
+
+"Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!".
+
+2. Modify the outputs above to use template literals to include the BMI values in the outputs.
+
+Example: "Mark's BMI (28.3) is higher than John's (23.9)!" or "John's BMI (29.1) is higher than Mark's (27)!".
+
+Note: Don't round the BMI values. Leave them as they are.
+*/
+const heightMark = 1.69;
+const heightJohn = 1.95;
+const massMark = 78;
+const massJohn = 92;
+
+const MarkBMI = massMark / (heightMark * heightMark);
+const JohnBMI = massJohn / (heightJohn * heightJohn);
+
+console.log(`Mark's BMI (${MarkBMI}) is higher than John's (${JohnBMI})!`);
+
+if (MarkBMI > JohnBMI) {
+    console.log(`Mark's BMI (${MarkBMI}) is higher than John's BMI (${JohnBMI})!`);
+} else {
+    console.log(`John's BMI (${JohnBMI}) is higher than Mark's BMI (${MarkBMI})!`);
+}
+
+/*
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+
+if (BMIJohn > BMIMark) {
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+} else {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+}
+*/
